e9c854ed8a6c96b843a649ed1ccfb7d9fce66bf7
5844joan23 <100005844.joan23@fje.edu>
2020-10-02 19:36:20 +0200

    subida 02/10/2020

--- services/connection.php
+++ services/connection.php
@@ -1,40 +1,8 @@
 <?php
-<<<<<<< HEAD
-// ESTILO POR PROCEDIMIENTOS
-=======
->>>>>>> 3298ff5bb5ba33f2bb30fc9c8edc101881d211c4
 
 $host = "localhost";
 $user = "root";
 $pass = "";
-<<<<<<< HEAD
-$db = "reread";
-
-// Crear la conexión
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-// Checkear la conexión
-if (!$conn) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "Error de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    exit;
-} else {
-	mysqli_set_charset($conn, "utf8");
-}
-
-/* //ESTILO ORIENTADO A OBJETOS
-// Crear la conexión
-$conn = new mysqli('localhost', 'root', '', 'imagenes');
-
-// Checkear la conexión
-if ($conn->connect_errno) {
-	die("Error: No se pudo conectar a MySQL: " . $conn->connect_errno);
-} else {
-	$conn->set_charset("utf8");
-}
-*/
-
-=======
 $db = "reread-php";
 
 $conn = mysqli_connect($host,$user, $pass, $db);
@@ -47,5 +15,4 @@ if (!$conn){
     mysqli_set_charset($conn, "utf8");
 }
 
->>>>>>> 3298ff5bb5ba33f2bb30fc9c8edc101881d211c4
 ?>
